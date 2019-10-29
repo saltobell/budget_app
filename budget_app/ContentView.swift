e72ffeb977d9c5bf2ba887df9f8fe5bb1e7d2c70
saltobell <saltobell@scu.edu>
2019-10-28 17:41:32 -0700

    Initial Commit

---
+++ budget_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  budget_app
+//
+//  Created by Sydney Altobell on 10/28/19.
+//  Copyright © 2019 CSCI 187. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
