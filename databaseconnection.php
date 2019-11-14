deeecb9b7d8bc8783f031f6b9f977886bb4055c5
ygarnica <40472742+ygarnica@users.noreply.github.com>
2019-11-14 14:19:40 -0800

    Add files via upload

---
+++ databaseconnection.php
@@ -0,0 +1,28 @@
+<?php
+//create connection to database
+$con=mysql_connect("localhost","root","root","budget_app");
+
+
+//check connection
+
+if (mysqli_connect_errno())
+{
+	echo "Failed to connect to MySQL: ".mysqli_connect_error();
+}
+
+$sql="SELECT * FROM Transactions";
+
+if($result = mysqli_query($con,$sql))
+{
+	$resultArray = array();
+	$tempArray = array();
+	while($row = $result->fetch_object())
+	{
+		tempArray = $row;
+		array_push($resultArray,$tempArray);
+	}
+	
+	echo json_encode($resultArray);
+}
+mysqli_close($con);
+?>
