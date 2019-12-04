7a1661493f5dd78df794c9405933623ddc5760e1
ygarnica <40472742+ygarnica@users.noreply.github.com>
2019-12-04 12:18:19 -0800

    Update DbOperations.php

--- DbOperations.php
+++ DbOperations.php
@@ -39,27 +39,6 @@ class DbOperations
 		}
 		return $return;
 	}
-	
-	public funciton createAccount($acc_name,$acc_type,$balance,$numOfAccts)
-	{
-		//is numofaccts user input?
-		$input = $this->conn->prepare("INSERT INTO Account(acc_name,acc_type,balance,numOfAccts_) values(?,?,?,?)");
-		//$input->bind_param()//missing something
-		$result=$input->execute();
-		$input->close();
-		if($result)
-		{
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	/*public function DeleteAccount($acc_name,$acc_type,$balance,$numOfAccts)
-	{
-		$input = $this->conn->prepare("DELETE acc_name,acc_type,balance FROM Account WHERE acc_ID = ?");//how do i know which account it is?
-		//also need to modify num of accounts
-	}*/
 	public function DeleteTranscation($transactionID,$amount,$date,$type_,$mehtod,$description)
 	{
 		$input = $this->conn->prepare("DELETE transactionID,amount,date,type_,method,description FROM Transaction WHERE transactionID = transactionIDID");
